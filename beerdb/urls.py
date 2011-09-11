ea3deb7962d5327fcb34df1d8110412f2c10f0d5
Dave Masse <dmasse@rudemoose.com>
2011-09-10 21:08:17 -0400

    Add user detail template. Set explicit context variable for beers and brewers.

--- beerdb/urls.py
+++ beerdb/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.models import User
 from django.views.generic import DetailView, ListView
 
 from beerdb.models import Beer, Brewer
 
 urlpatterns = patterns('',
+  # User ratings
+  (r'^user/(?P<slug>[^/]+)/$', DetailView.as_view(model=User, slug_field='username', context_object_name='user'), {}, 'beerdb_user_detail'),
+  
   # Beers
-  (r'^$', ListView.as_view(model=Beer), {}, 'beerdb_beer_list'),
-  (r'^(?P<brewer__slug>[^/]+)/(?P<slug>[^/]+)/$', DetailView.as_view(model=Beer), {}, 'beerdb_beer_detail'),
+  (r'^$', ListView.as_view(model=Beer, context_object_name='beers'), {}, 'beerdb_beer_list'),
+  (r'^(?P<brewer__slug>[^/]+)/(?P<slug>[^/]+)/$', DetailView.as_view(model=Beer, context_object_name='beer'), {}, 'beerdb_beer_detail'),
   
   # Brewers
-  (r'^brewers/$', ListView.as_view(model=Brewer), {}, 'beerdb_brewer_list'),
-  (r'^(?P<slug>[^/]+)/$', DetailView.as_view(model=Brewer), {}, 'beerdb_brewer_detail'),
+  (r'^brewers/$', ListView.as_view(model=Brewer, context_object_name='brewers'), {}, 'beerdb_brewer_list'),
+  (r'^(?P<slug>[^/]+)/$', DetailView.as_view(model=Brewer, context_object_name='brewer'), {}, 'beerdb_brewer_detail'),
 )
