1c95c667c331d050b132c3a174edad2588907ceb
Dave Masse <dmasse@rudemoose.com>
2011-09-11 20:41:12 -0400

    Add ways to delete a rating -- either explicitly or by having empty rating and note fields.

--- beerdb/forms.py
+++ beerdb/forms.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from beerdb.models import Beer, Rating, UserRating
 
 class UserRatingForm(forms.ModelForm):
+  delete = forms.BooleanField(required=False)
+  
   class Meta:
     model = UserRating
     fields = ('beer', 'user', 'rating', 'note')
@@ -11,6 +13,19 @@ class UserRatingForm(forms.ModelForm):
       'beer': forms.HiddenInput(),
       'user': forms.HiddenInput(),
     }
+  
+  def save(self, *args, **kwargs):
+    # Delete if user chose to delete rating or if no rating is set and note is empty
+    if (self.instance.id and self.cleaned_data['delete']) or (self.cleaned_data['rating'] == None and len(self.cleaned_data['note']) == 0):
+      # Delete the current instance
+      self.instance.delete()
+      # Return an empty instance
+      instance = UserRating.objects.none()
+    else:
+      # Default instance creation/update
+      instance = super(UserRatingForm, self).save(*args, **kwargs)
+    
+    return instance
 
 class SearchForm(forms.Form):
   query = forms.CharField(max_length=100)
