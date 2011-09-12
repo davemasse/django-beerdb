c40d8b44a8dcf89b67097e049260e578bd4205ce
Dave Masse <dmasse@rudemoose.com>
2011-09-11 21:51:15 -0400

    Warn the user if no rating or note are provided.

--- beerdb/forms.py
+++ beerdb/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 from beerdb.models import Beer, Rating, UserRating
 
@@ -14,11 +15,21 @@ class UserRatingForm(forms.ModelForm):
       'user': forms.HiddenInput(),
     }
   
+  def clean(self):
+    cleaned_data = self.cleaned_data
+    
+    # If fields are empty and deletion wasn't checked
+    if cleaned_data['rating'] == None and len(cleaned_data['note']) == 0 and cleaned_data['delete'] == False:
+      raise forms.ValidationError(_('Please provide a rating or note for this beer.'))
+    
+    return cleaned_data
+  
   def save(self, *args, **kwargs):
     # Delete if user chose to delete rating or if no rating is set and note is empty
-    if (self.instance.id and self.cleaned_data['delete']) or (self.cleaned_data['rating'] == None and len(self.cleaned_data['note']) == 0):
+    if self.instance.id and ((self.cleaned_data['delete']) or (self.cleaned_data['rating'] == None and len(self.cleaned_data['note']) == 0)):
       # Delete the current instance
       self.instance.delete()
+      
       # Return an empty instance
       instance = UserRating.objects.none()
     else:
